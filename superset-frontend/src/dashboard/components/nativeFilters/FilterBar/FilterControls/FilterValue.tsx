dd06e8b67410a9616b4e79761c1bd86f6c579deb
Malov Gleb <gleb.malov@arsis.ru>
2025-03-07 15:50:13 +0500

    Add postMessage for get chart

--- superset-frontend/src/dashboard/components/nativeFilters/FilterBar/FilterControls/FilterValue.tsx
+++ superset-frontend/src/dashboard/components/nativeFilters/FilterBar/FilterControls/FilterValue.tsx
@@ -179,6 +179,10 @@ const FilterValue: FC<FilterControlProps> = ({
         ownState: filterOwnState,
       })
         .then(({ response, json }) => {
+          window.parent.postMessage(
+            { type: 'formData', key: newFormData },
+            '*',
+          );
           if (isFeatureEnabled(FeatureFlag.GlobalAsyncQueries)) {
             // deal with getChartDataRequest transforming the response data
             const result = 'result' in json ? json.result[0] : json;
